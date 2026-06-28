dadaf35cce4898038fd816e2c55cd49a8246b7b9
YuktaAgnihotri <yuktaagnihotri8585@gmail.com>
2026-06-28 19:35:47 +0530

    added db.config.ts

--- db.config.ts
+++ db.config.ts
@@ -1,8 +1,8 @@
-// import { configDotenv } from "dotenv";
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// const adapter = new PrismaPg({
-//    connectionString: process.env.DATABASE_URL,
-// });
-// export const prisma = new PrismaClient({adapter});
+import { configDotenv } from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({
+   connectionString: process.env.DATABASE_URL,
+});
+export const prisma = new PrismaClient({adapter});
 
